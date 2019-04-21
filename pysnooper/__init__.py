8d6b7cadaef3c6e8c29c8ebc442547c1d2e12817
Ram Rachum <ram@rachum.com>
2019-04-21 22:13:22 +0300

    Add Python 2 compatibility lines

--- pysnooper/__init__.py
+++ pysnooper/__init__.py
@@ -1,4 +1,8 @@
 # Copyright 2019 Ram Rachum.
 # This program is distributed under the MIT license.
 
+import future.standard_library
+future.standard_library.install_aliases()
+del future
+
 from .pysnooper import snoop
