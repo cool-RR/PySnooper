61fd0a526284c59bb9fc48719ad64bdbf745ec28
Ram Rachum <ram@rachum.com>
2019-04-24 22:14:36 +0300

    Bump version

--- pysnooper/__init__.py
+++ pysnooper/__init__.py
@@ -7,7 +7,7 @@ import collections
 __VersionInfo = collections.namedtuple('VersionInfo',
                                        ('major', 'minor', 'micro'))
 
-__version_info__ = __VersionInfo(0, 0, 16)
+__version_info__ = __VersionInfo(0, 0, 17)
 __version__ = '.'.join(map(str, __version_info__))
 
 del collections, __VersionInfo # Avoid polluting the namespace
