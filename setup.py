54c138f69259b8df94f43e55ff2d0c5f9ad425df
Peter Bittner <django@bittner.it>
2019-04-24 12:44:34 +0200

    Add platform trove classifiers

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Debuggers',
