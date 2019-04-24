665785db9266fc5f1d766bdc3c5ba37dd94c856c
Ram Rachum <ram@rachum.com>
2019-04-24 13:57:38 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read_file(filename):
 
 setuptools.setup(
     name='PySnooper',
-    version='0.0.12',
+    version='0.0.13',
     author='Ram Rachum',
     author_email='ram@rachum.com',
     description="A poor man's debugger for Python.",
