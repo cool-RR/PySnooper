7e845b8f493d82c9e6f6f4d200136bb8a956dcfc
Andreas van Cranenburgh <andreas@unstable.nl>
2019-05-03 21:19:16 +0200

    add usage to package docstring; fixes #83

--- pysnooper/__init__.py
+++ pysnooper/__init__.py
@@ -1,5 +1,20 @@
 # Copyright 2019 Ram Rachum and collaborators.
 # This program is distributed under the MIT license.
+"""PySnooper - Never use print for debugging again
+
+Usage:
+
+    import pysnooper
+
+    @pysnooper.snoop()
+    def number_to_bits(number):
+        ...
+
+A log will be written to stderr showing the lines executed and variables
+changed in the decorated function.
+
+For more information, see https://github.com/cool-RR/PySnooper
+"""
 
 from .pysnooper import snoop
 from .variables import Attrs, Exploding, Indices, Keys
