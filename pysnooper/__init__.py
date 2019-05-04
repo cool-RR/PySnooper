3723963cc9ee7f42609dfbe002c0f910aab99bde
Ram Rachum <ram@rachum.com>
2019-05-04 09:36:43 +0300

    Bump version

--- pysnooper/__init__.py
+++ pysnooper/__init__.py
@@ -1,20 +1,21 @@
 # Copyright 2019 Ram Rachum and collaborators.
 # This program is distributed under the MIT license.
-"""PySnooper - Never use print for debugging again
+'''
+PySnooper - Never use print for debugging again
 
 Usage:
 
     import pysnooper
 
     @pysnooper.snoop()
-    def number_to_bits(number):
+    def your_function(x):
         ...
 
 A log will be written to stderr showing the lines executed and variables
 changed in the decorated function.
 
 For more information, see https://github.com/cool-RR/PySnooper
-"""
+'''
 
 from .pysnooper import snoop
 from .variables import Attrs, Exploding, Indices, Keys
@@ -23,7 +24,7 @@ import collections
 __VersionInfo = collections.namedtuple('VersionInfo',
                                        ('major', 'minor', 'micro'))
 
-__version__ = '0.0.30'
+__version__ = '0.0.31'
 __version_info__ = __VersionInfo(*(map(int, __version__.split('.'))))
 
 del collections, __VersionInfo # Avoid polluting the namespace
