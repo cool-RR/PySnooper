814abc34a098c1b98cb327105ac396f985d2413e
Ram Rachum <ram@rachum.com>
2019-07-30 10:56:58 +0300

    Support single tuple to `custom_repr`, fix #144

--- tests/test_pysnooper.py
+++ tests/test_pysnooper.py
@@ -1174,6 +1174,30 @@ def test_custom_repr():
         )
     )
 
+def test_custom_repr_single():
+    string_io = io.StringIO()
+
+    @pysnooper.snoop(string_io, custom_repr=(list, lambda l: 'foofoo!'))
+    def sum_to_x(x):
+        l = list(range(x))
+        return 7
+
+    result = sum_to_x(10000)
+
+    output = string_io.getvalue()
+    assert_output(
+        output,
+        (
+            VariableEntry('x', '10000'),
+            CallEntry(),
+            LineEntry(),
+            VariableEntry('l', 'foofoo!'),
+            LineEntry(),
+            ReturnEntry(),
+            ReturnValueEntry('7'),
+        )
+    )
+
 
 def test_disable():
     string_io = io.StringIO()
